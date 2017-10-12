0a9fe0a3404b6aeee5665e85e557961060fb0a9d
kmac007 <robbchan@foxmail.com>
2017-10-12 13:40:48 +0800

    首页数据绑定，完成星星模版

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,3 +1,5 @@
+var util = require('../../utils/util.js');
+var app = getApp()
 // pages/movies/movies.js
 Page({
 
@@ -5,14 +7,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    in_theaters: {},
+    coming_soon: {},
+    top250: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.showLoading({
+      title: '加载中',
+    })
+    var api = 'https://api.douban.com/v2/movie/'
+    this.getMoviesList(`${api}in_theaters`)
+    this.getMoviesList(`${api}coming_soon`)
+    this.getMoviesList(`${api}top250`)
   },
 
   /**
@@ -72,5 +82,55 @@ Page({
   //更多电影
   handleMoreMovie() {
     console.log('更多电影')
+  },
+
+  //获取电影条目信息
+  getMoviesList(url) {
+    var that = this
+    wx.request({
+      url: url,
+      header: {
+        'content-type': 'json'
+      },
+      success(res) {
+        that.handleMoviesList(res.data)
+      }
+    })
+  },
+  //将星星对应字符串转换为数组
+  handleStars(data) {
+    for (var i = 0; i < data.subjects.length; i++) {
+      data.subjects[i].rating.stars = util.convertStarsToArray(data.subjects[i].rating.stars)
+    }
+  },
+  //数据处理函数
+  handleMoviesList(data) {
+    switch (data.title) {
+      case '正在上映的电影-北京':
+        data.title = '正在热映'
+        data.subjects = data.subjects.slice(0, 7)
+        this.handleStars(data)
+        this.setData({
+          in_theaters: data
+        })
+        break;
+      case '即将上映的电影':
+        data.title = '近期上映'
+        data.subjects = data.subjects.slice(0, 7)
+        this.handleStars(data)
+        this.setData({
+          coming_soon: data
+        })
+        break;
+      case '豆瓣电影Top250':
+        data.title = 'Top250'
+        data.subjects = data.subjects.slice(0, 7)
+        this.handleStars(data)
+        this.setData({
+          top250: data
+        })
+        break;
+    }
+    wx.hideLoading()
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,23 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const convertStarsToArray = stars => {
+  const frontNum = stars.toString().substr(0, 1)
+  const backNum = stars.toString().substr(1, 2)
+  var arr = []
+  for (var i = 0; i < 5; i++) {
+    if (i < frontNum) {
+      arr.push(1)
+    } else if (backNum!=0 && !arr.includes(2)) {
+      arr.push(2)
+    } else {
+      arr.push(0)
+    }
+  }
+  return arr
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  convertStarsToArray: convertStarsToArray
 }
